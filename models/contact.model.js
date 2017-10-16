5871ea17de5f1c5cba6838b98ad647f34de817af
Duex <deux02030@gmail.com>
2017-10-16 07:26:38 -0400

    docs: create models folder
    Create a models folder to store models.

---
+++ models/contact.model.js
@@ -0,0 +1,11 @@
+"use strict";
+var Contact = (function () {
+    function Contact(firstName, lastName, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+    return Contact;
+}());
+exports.Contact = Contact;
+//# sourceMappingURL=contact.model.js.map
