a729f8ae8839d69a56788004f14294974f81c783
Duex <deux02030@gmail.com>
2017-10-26 14:38:46 -0400

    docs: add contact components

--- contact/app.contactComponent.js
+++ contact/app.contactComponent.js
@@ -9,7 +9,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
-var contact_model_1 = require("../contact.model");
+var contact_model_1 = require("../models/contact.model");
 var contactComponent = (function () {
     function contactComponent() {
         this.model = new contact_model_1.Contact("", "", "");
--- contact/app.contactComponent.ts
+++ contact/app.contactComponent.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Contact } from '../contact.model';
+import { Contact } from '../models/contact.model';
 
 @Component({
   //selector: 'contact-form',
